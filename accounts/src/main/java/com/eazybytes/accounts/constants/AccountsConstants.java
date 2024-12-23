fea561de76cc5b40a32099fc5dc749c74326f299
LizzzG <xiguan710@gmail.com>
2024-12-23 16:15:08 -0500

    add dto repository and constants

---
+++ accounts/src/main/java/com/eazybytes/accounts/constants/AccountsConstants.java
@@ -0,0 +1,16 @@
+package com.eazybytes.accounts.constants;
+
+public class AccountsConstants {
+    private AccountsConstants() {
+        // restrict instantiation
+    }
+
+    public static final String SAVINGS = "Savings";
+    public static final String ADDRESS = "123 Main Street, New York";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Account Created Successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request processed Successfully";
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGE_500 = "Internal Server Error. Please try again or contact Dev team";
+}
